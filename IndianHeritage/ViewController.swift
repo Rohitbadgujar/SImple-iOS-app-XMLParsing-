fb3da1e642ff3e44607a51c22c409f2dd4497569
Rohit <tyson@rohits-mac.local>
2019-03-07 15:40:31 -0800

    Initial Commit

---
+++ IndianHeritage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IndianHeritage
+//
+//  Created by Rohit on 12/16/1397 AP.
+//  Copyright © 1397 Rohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
